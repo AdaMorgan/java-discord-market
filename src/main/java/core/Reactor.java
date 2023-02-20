321a3c9535870a14fa8bdc6bf1329eceabbb69c3
adamorgan <unddeus@gmail.com>
2023-02-20 08:05:37 +0300

    Init

--- src/main/java/core/Reactor.java
+++ src/main/java/core/Reactor.java
@@ -5,9 +5,13 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 
 public class Reactor {
-    public static void main(String[] args) {
-        Flux.generate(synchronous -> synchronous.next("Work!"))
+    public static final double POSITIVE_INFINITY = 1.0 / 0.0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Flux.generate(synchronous -> synchronous.next("Ping!"))
                 .delayElements(Duration.ofSeconds(5))
                 .subscribe(System.out::println);
+
+        Thread.sleep((long) POSITIVE_INFINITY);
     }
 }
